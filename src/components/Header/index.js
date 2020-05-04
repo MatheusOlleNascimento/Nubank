5ba539c360ffc2e8d832042f9cee355d1996c0f1
Matheus <matheusollenascimento@gmail.com>
2020-05-04 14:37:53 -0300

    Logo e tabs OK

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import logo from '~/assets/Nubank_Logo.png';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from 'react-native';
+import { Container, Top, Logo, Title } from './styles';
 
 export default function Header() {
     return (
         <Container>
             <Top>
-                <Logo source={}/>
+                <Logo source={logo}/>
                 <Title>Matheus</Title>
             </Top>
+            <Icon name="keyboard-arrow-down" size={20} color="#FFF"/>
         </Container>
     );
 }
